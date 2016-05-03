a5a6606a7ef5792a98ee6cd9a1374de9f3519518
王森 <wangs@tongxinyiliao.com>
2016-05-03 18:03:37 +0800

    try mac develop

---
+++ golds/SWNotification.h
@@ -0,0 +1,13 @@
+//
+//  SWNotification.h
+//  golds
+//
+//  Created by zhangwt on 16/5/3.
+//  Copyright © 2016年 wangsen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SWNotification : NSObject
+
+@end
---
+++ golds/SWNotification.m
@@ -0,0 +1,13 @@
+//
+//  SWNotification.m
+//  golds
+//
+//  Created by zhangwt on 16/5/3.
+//  Copyright © 2016年 wangsen. All rights reserved.
+//
+
+#import "SWNotification.h"
+
+@implementation SWNotification
+
+@end

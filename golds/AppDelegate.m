a4dc44f000a84cff0d5f0475dccd97b5d8238a16
王森 <wangs@tongxinyiliao.com>
2016-05-10 13:51:06 +0800

    gold

--- golds/AppDelegate.m
+++ golds/AppDelegate.m
@@ -16,16 +16,26 @@
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
     // Insert code here to initialize your application
-    [[SWStatusItem shareInstance] showIcon:[NSImage imageNamed:@"icon"] title:@"1234.56"];
-    [[SWStatusItem shareInstance] addSelect:@selector(clicStatusBarIcon:) withTarget:self];
+//    [[SWStatusItem shareInstance] showIcon:[NSImage imageNamed:@"icon"] title:@"1234.56"];
+//    [[SWStatusItem shareInstance] addSelect:@selector(clicStatusBarIcon:) withTarget:self];
+    NSTimer *timer = [NSTimer timerWithTimeInterval:5 target:self selector:@selector(getJSBPrice:) userInfo:nil repeats:YES];
+    [[NSRunLoop mainRunLoop] addTimer:timer forMode:NSDefaultRunLoopMode];
 }
 
 - (void)applicationWillTerminate:(NSNotification *)aNotification {
     // Insert code here to tear down your application
 }
 
-- (void)clicStatusBarIcon: (id)sender{
-    NSLog(@"I be clicked");
+- (void)getJSBPrice: (id)sender{
+    NSLog(@"123");
+    NSMutableURLRequest *request = [[NSMutableURLRequest alloc] init];
+    [request setURL:[NSURL URLWithString:@"https://login.vip9999.com/?s=api-getgold"]];
+    [request setHTTPMethod:@"GET"];
+    [request setTimeoutInterval:60];
+    NSData *data = [NSURLConnection sendSynchronousRequest:request returningResponse:Nil error:nil];
+    //NSString* dataStr = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+    NSDictionary *resultDic = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error:nil];
+    [[SWStatusItem shareInstance] showTitle:resultDic[@"results"][@"buy"]];
 }
 
 @end
